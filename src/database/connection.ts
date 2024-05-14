cf4977f529e0626dd17a4b4ad357060ae7237199
Nguyen Phu Hong Loan <loannph@stageit.app>
2024-05-14 17:05:05 +0700

    improve scripts

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,17 +1,21 @@
+import path from 'path';
 import { DataSource } from 'typeorm';
 import { config } from '../config/configENV';
 const AppDataSource = new DataSource({
-    type: "mysql",
+    type: "mariadb",
     host: config.DB_HOST,
     port: config.DB_PORT,
     username: config.DB_USER,
     password: config.DB_PASSWORD,
     database: config.DB_NAME,
-    entities: ["src/modules/**/*.entity{.ts,.js}"],
-    migrations: ["src/migration/*.ts"],
-    synchronize: true,
+    entities: [path.join(__dirname, "../modules/**/*.entity{.ts,.js}")],
+    migrations: [path.join(__dirname, "./src/migration/*.ts,.js")],
+    migrationsTableName: 'migrate',    
+    // synchronize: true,
     //logging: true,
 });
+
+console.log(path.join(__dirname, "/migration/*.ts,.js"));
 
 export default AppDataSource;
 
